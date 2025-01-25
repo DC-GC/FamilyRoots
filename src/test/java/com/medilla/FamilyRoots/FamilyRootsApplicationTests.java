7bf0b753ce18d4cf5fb2872e27e2652448b02746
Dan Medilla <danmedilla@gmail.com>
2025-01-25 16:16:30 +0800

    first commit.

---
+++ src/test/java/com/medilla/FamilyRoots/FamilyRootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilla.FamilyRoots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyRootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
